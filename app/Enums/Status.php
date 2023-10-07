4bd5b8b1192a0700d3c3d08b8d0d4668ac707462
cassioleite <cassiodegoes@live.com>
2023-10-07 17:16:21 -0300

    task:(APP) criacao models de Schedule e Type e criacao de Enum de Status.

---
+++ app/Enums/Status.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum Status: string
+{
+    CASE OPEN = 'O';
+    CASE CLOSED = 'C';
+}
---
+++ app/Models/Schedule.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Status;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Schedule extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type_id',
+        'description',
+        'starting_at',
+        'conclusion_at',
+        'due_at',
+        'status'
+    ];
+
+    protected $casts = [
+        'status' => Status::class
+    ];
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function type(): HasOne
+    {
+        return $this->hasOne(Type::class);
+    }
+}
